47fccf01efaf31821e73b835305b43154a568fb5
tomdavison96 <tomdavison3@gmail.com>
2022-12-20 23:42:58 +0000

    day 8 part 2 solution

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -1,4 +1,5 @@
 from itertools import product
+from typing import List
 
 with open('day_8/input_8.txt') as f:
     rows = f.readlines()
@@ -40,3 +41,46 @@ for i, j in product(range(n_rows), range(n_columns)):
     visible_count += visible(row_index=i, column_index=j, tree_row=row, tree_column=column)
 
 print(f'Of {n_rows * n_columns} total trees, {visible_count} are visible')
+
+
+def tree_counter(trees: List[str], main_tree: str) -> int:
+    count = 0
+    for tree in trees:
+        count += 1
+        if int(tree) >= int(main_tree):
+            break
+    return count
+
+
+def visible_before_count(trees: str, index: int) -> int:
+    if index == 0:
+        return 0
+    trees_to_consider = list(reversed(trees[:index]))
+    main_tree = trees[index]
+    return tree_counter(trees_to_consider, main_tree)
+
+
+def visible_after_count(trees: str, index: int) -> int:
+    if index == len(trees) - 1:
+        return 0
+    trees_to_consider = list(trees[index + 1:])
+    main_tree = trees[index]
+    return tree_counter(trees_to_consider, main_tree)
+
+
+def scenic_score(row_index: int, column_index: int, tree_row: str, tree_column: str) -> int:
+    assert tree_row[column_index] == tree_column[row_index]
+    visible_above_count = visible_before_count(tree_column, row_index)
+    visible_below_count = visible_after_count(tree_column, row_index)
+    visible_left_count = visible_before_count(tree_row, column_index)
+    visible_right_count = visible_after_count(tree_row, column_index)
+    return visible_above_count * visible_below_count * visible_left_count * visible_right_count
+
+
+scenic_scores = []
+for i, j in product(range(n_rows), range(n_columns)):
+    row = rows[i]
+    column = columns[j]
+    scenic_scores += [scenic_score(row_index=i, column_index=j, tree_row=row, tree_column=column)]
+
+print(f'\nOf {n_rows * n_columns} total trees, the highest scenic score is {max(scenic_scores)}')
