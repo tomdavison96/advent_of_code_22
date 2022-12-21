a4e2beb6bd42d23c276435aaa7c6a1c4c9569660
tomdavison96 <tomdavison3@gmail.com>
2022-12-21 20:07:37 +0000

    day 9 part 1 solution

---
+++ day_9/day_9.py
@@ -0,0 +1,83 @@
+from typing import Tuple
+import math
+
+with open('day_9/input_9.txt') as f:
+    instructions = [tuple(line.split()) for line in f.readlines()]
+
+instructions = [tuple([instruction[0], int(instruction[1])]) for instruction in instructions]
+
+
+def move_head(current_x: int, current_y: int, direction: str) -> Tuple[int, int]:
+    """Move the head of the rope in the given direction by 1 unit and return the new x, y coordinates"""
+    if direction == 'L':
+        current_x -= 1
+    elif direction == 'R':
+        current_x += 1
+    elif direction == 'U':
+        current_y += 1
+    elif direction == 'D':
+        current_y -= 1
+    else:
+        raise ValueError(f'Unexpected direction provided: {direction}')
+    return current_x, current_y
+
+
+def move_tail(tail_x: int, tail_y: int, head_x: int, head_y: int) -> Tuple[int, int]:
+    """Move the tail of the rope based on the current position of the head of the rope"""
+    x_diff = head_x - tail_x
+    y_diff = head_y - tail_y
+    assert max([abs(x_diff), abs(y_diff)]) <= 2, f'Pre tail move {x_diff=}, {y_diff=}'
+    if x_diff == 0 and abs(y_diff) == 2:
+        tail_y += math.copysign(1, y_diff)
+    elif abs(x_diff) == 2 and y_diff == 0:
+        tail_x += math.copysign(1, x_diff)
+    elif (abs(x_diff) == 1 and abs(y_diff) == 2) or (abs(y_diff) == 1 and abs(x_diff) == 2):
+        tail_x += math.copysign(1, x_diff)
+        tail_y += math.copysign(1, y_diff)
+    elif (x_diff == 0 and y_diff == 0) or (abs(x_diff) == 1 and y_diff == 0) or (x_diff == 0 and abs(y_diff) == 1) or (abs(x_diff) == 1 and abs(y_diff) == 1):
+        pass
+    else:
+        raise ValueError(f'Unexpected result. Tail Coords: ({tail_x}, {tail_y}), Head Coords: ({head_x, head_y}). '
+                         f'{x_diff=}, {y_diff=}')
+    x_diff = head_x - tail_x
+    y_diff = head_y - tail_y
+    assert max([abs(x_diff), abs(y_diff)]) <= 1, f'Post tail move {x_diff=}, {y_diff=}'
+    return tail_x, tail_y
+
+
+x, y = 0, 0
+head_locations = [(x, y)]
+tail_x, tail_y = 0, 0
+tail_locations = [(tail_x, tail_y)]
+for instruction in instructions:
+    direction, distance = instruction
+    for i in range(distance):
+        x, y = move_head(x, y, direction)
+        head_locations += [(x, y)]
+        tail_x, tail_y = move_tail(tail_x, tail_y, x, y)
+        tail_locations += [(tail_x, tail_y)]
+
+print(f'After following all instructions the head was moved {len(head_locations)} times '
+      f'and the tail visited {len(set(tail_locations))} unique locations')
+
+
+x, y = 0, 0
+head_locations = [(x, y)]
+tail_locations = [(x, y)]
+rope_length = 10
+rope_positions = [(x, y)] * rope_length
+for instruction in instructions:
+    direction, distance = instruction
+    for i in range(distance):
+        x, y = rope_positions.pop(0)
+        x, y = move_head(x, y, direction)
+        head_locations += [(x, y)]
+        rope_positions.insert(0, (x, y))
+        assert len(rope_positions) == 10
+        for knot in range(1, rope_length):  # Iterate through other parts of the rope and treat each one as a 'tail'
+            head_part_x, head_part_y = rope_positions[knot - 1]
+            tail_part_x, tail_part_y = rope_positions.pop(knot)
+            tail_part_x, tail_part_y = move_tail(tail_part_x, tail_part_y, head_part_x, head_part_y)
+            rope_positions.insert(knot, (tail_part_x, tail_part_y))
+            assert len(rope_positions) == 10
+        tail_locations += [rope_positions[-1]]
